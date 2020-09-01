b2b4990c476034f9e1686e7b34043adb88cb7be3
Poojit Hegde <phegde8@stanford.edu>
2020-08-31 20:55:05 -0400

    training data code
    code to generate training data for a^2+b^2=1

---
+++ a2b1/nettest.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+
+sizeone = (1000,1)
+sizetwo = (1000,1)
+marginoferror = 0.01
+
+agen = np.random.lognormal(0, 1, size1)
+bgen = np.random.lognormal(0, 1, size1)
+cgen = np.zeros_like(agen)
+boolArrgen = (agen**2+bgen**2-1<=marginoferror)
+cgen[boolArrgen] = 1
+
+atrues = np.random.random(sizetwo)
+btruesquares = 1-atrues**2 + (2*np.random.random()-1) * marginoferror
+underZeroBool = btruesquares < 0
+btruesquares[underZeroBool] = 0
+btrues = np.sqrt(btruesquares)
+boolArrtrues = (atrues**2+btrues**2-1 <= marginoferror)
+ctrues = np.zeros_like(atrues)
+ctrues[boolArrtrues] = 1
+
+afin = np.concatenate((agen, atrues), axis = 0)
+bfin = np.concatenate((bgen, btrues), axis = 0)
+cfin = np.concatenate((cgen, ctrues), axis = 0)
