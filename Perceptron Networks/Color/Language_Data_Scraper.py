b43c7dd998f3f33ce585eb7859d1193dc3b8aa9d
srangan24 <srinath.rangan@gmail.com>
2021-02-06 12:50:23 -0500

    intergrating auto network

--- Perceptron Networks/Color/Language_Data_Scraper.py
+++ Perceptron Networks/Color/Language_Data_Scraper.py
@@ -1,29 +1,29 @@
 import pandas as pd
 
-# Read and clean data
-# Access to color coordinates
-term_data = pd.read_csv('Perceptron Networks/Color/term.txt', sep="\t", header=None)
-term_data.columns = ["#Lnum", "#snum", "#cnum", "Term Abbrev"]
-cnum_data = pd.read_csv('Perceptron Networks/Color/cnum-vhcm-lab-new.txt', sep="\t")
-
-locations = cnum_data[['#cnum']]
-locations['Normalized-L'] = (cnum_data['L*'] - cnum_data['L*'].mean())/(cnum_data['L*'] - cnum_data['L*'].mean()).max()
-locations['Normalized-a'] = (cnum_data['a*'] - cnum_data['a*'].mean())/(cnum_data['a*'] - cnum_data['a*'].mean()).std() * 1/2
-locations['Normalized-b'] = (cnum_data['b*'] - cnum_data['b*'].mean())/(cnum_data['b*'] - cnum_data['b*'].mean()).std() * 1/2
-
-locations = locations.sort_values('#cnum')
-chip_num = list(locations['#cnum'])
-lab_norm = [[row[2], row[3], row[4]] for row in locations.itertuples()]
-
 # Filters dataset for specific language for neural network input
 # Access to chip percentages and number of colors
 class LanguageData:
 
     def __init__(self, language_number):
         self.language_number = language_number
-    
+
+        # Read and clean data
+        # Access to color coordinates
+        self.term_data = pd.read_csv('Perceptron Networks/Color/term.txt', sep="\t", header=None)
+        self.term_data.columns = ["#Lnum", "#snum", "#cnum", "Term Abbrev"]
+        cnum_data = pd.read_csv('Perceptron Networks/Color/cnum-vhcm-lab-new.txt', sep="\t")
+
+        locations = cnum_data[['#cnum']]
+        locations['Normalized-L'] = (cnum_data['L*'] - cnum_data['L*'].mean())/(cnum_data['L*'] - cnum_data['L*'].mean()).max()
+        locations['Normalized-a'] = (cnum_data['a*'] - cnum_data['a*'].mean())/(cnum_data['a*'] - cnum_data['a*'].mean()).std() * 1/2
+        locations['Normalized-b'] = (cnum_data['b*'] - cnum_data['b*'].mean())/(cnum_data['b*'] - cnum_data['b*'].mean()).std() * 1/2
+
+        locations = locations.sort_values('#cnum')
+        self.chip_num = list(locations['#cnum'])
+        self.lab_norm = [[row[2], row[3], row[4]] for row in locations.itertuples()]
+
     def language_data(self):
-        isolation = term_data[term_data.get('#Lnum').eq(self.language_number)]
+        isolation = self.term_data[self.term_data.get('#Lnum').eq(self.language_number)]
         isolation_grouped = isolation.groupby('#cnum')['Term Abbrev'].apply(list)
         unique_symbols = list(isolation['Term Abbrev'].unique())
         chip_abbrev_percentage = [[(isolation_grouped[i + 1].count(abbrev) / len(isolation_grouped[i + 1])) for abbrev in unique_symbols] for i in range(len(isolation_grouped))]
@@ -32,13 +32,13 @@ class LanguageData:
         result.index += 1
         result.index.name = '#cnum'
         result.columns = unique_symbols
-        return isolation, result
+        return result
     
     def chip_norm(self):
         percentages = []
-        language, distribution = self.language_data()
-        for value in chip_num:
-            percentages.append((distribution.loc[language["#cnum"] == value]).values.tolist()[0])
+        distribution = self.language_data()
+        for value in self.chip_num:
+            percentages.append(distribution.loc[value,:].values.tolist())
         return percentages
 
     def colors_num(self):
